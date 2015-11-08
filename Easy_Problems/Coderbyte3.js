5d1b52efb20a569b3cbf0ce290d2c7b3d0b5aff0
annadeu3 <annadeu3@gmail.com>
2015-11-08 11:34:42 -0500

    problem 3

--- Easy_Problems/Coderbyte3.js
+++ Easy_Problems/Coderbyte3.js
@@ -1,3 +1,11 @@
 function LongestWord(sen) {
-	
+	var array = sen.split(" ");
+
+	for (var i = 0; i < array.length; i++) {
+		array[i] = array[i].replace(/\W/g, "");
+	}
+	array = array.sort (function(a, b) {
+		return b.length - a.length;});
+	return array[0];
 };
+console.log(LongestWord("this is Sparta"))
