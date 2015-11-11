b90caca708da1839d14bd1403d949859c3103432
Anna Deu <annadeu3@gmail.com>
2015-11-11 16:09:43 -0500

    Update Coderbyte1.js
    removed quotations

--- Easy_Problems/Coderbyte1.js
+++ Easy_Problems/Coderbyte1.js
@@ -2,7 +2,7 @@ function FirstReverse(str) {
 	var newStr = str.split("");
 
 	for(var i = 0; i < newStr.length; i++) {
-		return newStr.reverse("").join("");
+		return newStr.reverse().join("");
 	}
 };
 console.log(FirstReverse("Coderbyte is cool"));
